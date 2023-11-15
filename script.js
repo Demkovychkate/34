f85e80db15c54bba3a5c93d801b09a4bd0662a82
Demkovychkate <Panchevakate1804@gmail.com>
2023-11-15 17:40:27 +0200

    creating block

---
+++ script.js
@@ -0,0 +1,19 @@
+const block = document.createElement('div');
+block.className = 'block';
+document.body.appendChild(block);
+
+const STEP = 50;
+
+const operations = {
+    37: (block) => block.style.left = `${parseInt(block.style.left || 0) - STEP}px `,
+    38: (block) => block.style.top = `${parseInt(block.style.top || 0) - STEP}px `,
+    39: (block) =>block.style.left = `${parseInt(block.style.left || 0) + STEP}px `,
+    40: (block) => block.style.top = `${parseInt(block.style.top || 0) + STEP}px `,
+}
+
+function setStepToBlock (e) {
+    operations[e.keyCode] (block);
+}
+
+document.addEventListener('keydown', setStepToBlock);
+   
