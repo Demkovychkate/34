0b7c7e2efd9d640426a914f82224021db3356a5f
Demkovychkate <Panchevakate1804@gmail.com>
2023-11-16 14:23:51 +0200

    Add setInterval

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ document.body.appendChild(block);
 const STEP = 50;
 
 const operations = {
-    37: (block) => block.style.left = `${parseInt(block.style.left || 0) - STEP}px `,
-    38: (block) => block.style.top = `${parseInt(block.style.top || 0) - STEP}px `,
+    37: (block) => block.style.left = `${parseInt(block.style.left || 0) - STEP}px `,  
     39: (block) =>block.style.left = `${parseInt(block.style.left || 0) + STEP}px `,
     40: (block) => block.style.top = `${parseInt(block.style.top || 0) + STEP}px `,
 }
 
-function setStepToBlock (e) {
-    operations[e.keyCode] (block);
+function setStepToBlock(e) {
+    operations[e.keyCode](block);
 }
 
 document.addEventListener('keydown', setStepToBlock);
+
+
+setInterval(() => {
+    setStepToBlock({ keyCode: 40});  
+}, 1000);
    
