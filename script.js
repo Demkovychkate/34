5c180e5766b59903c6d9c1d40e811ffbf36c5c1e
Demkovychkate <Panchevakate1804@gmail.com>
2023-11-16 23:18:04 +0200

    First Part

--- script.js
+++ script.js
@@ -2,21 +2,58 @@ const block = document.createElement('div');
 block.className = 'block';
 document.body.appendChild(block);
 
+const input = document.createElement('input');
+input.className = 'input';
+document.body.appendChild(input);
+
+const maxX = window.innerWidth - block.offsetWidth;
+const maxY = window.innerHeight - block.offsetHeight;
 const STEP = 50;
 
+let score = 0; 
+
 const operations = {
-    37: (block) => block.style.left = `${parseInt(block.style.left || 0) - STEP}px `,  
-    39: (block) =>block.style.left = `${parseInt(block.style.left || 0) + STEP}px `,
-    40: (block) => block.style.top = `${parseInt(block.style.top || 0) + STEP}px `,
+    37: () => {
+        const leftPosition = parseInt(block.style.left || 0);
+        if (leftPosition - STEP >= 0 && isAboveBottomEdge()) {
+            block.style.left = `${leftPosition - STEP}px`;
+        } showScore();
+    },
+    39: () => {
+        const leftPosition = parseInt(block.style.left || 0);
+        if (leftPosition + STEP <= maxX && isAboveBottomEdge()) {
+            block.style.left = `${leftPosition + STEP}px`;
+        } showScore();
+    },
+    40: () => {
+         topPosition = parseInt(block.style.top || 0);
+        if (topPosition + STEP <= maxY) {
+            block.style.top = `${topPosition + STEP}px`;
+        } 
+    }
+};
+
+function isAboveBottomEdge() {
+    const topPosition = parseInt(block.style.top || 0);
+    return topPosition + block.offsetHeight < maxY;
 }
 
-function setStepToBlock(e) {
-    operations[e.keyCode](block);
+function showScore() {
+    if (isAboveBottomEdge()) {
+        score += 1;
+        input.value = `Бали: ${score}`;
+    }
 }
 
-document.addEventListener('keydown', setStepToBlock);
+function handleKeyPress(e) {
+    if (operations[e.keyCode]) {
+        operations[e.keyCode]();
+    }
+}
 
+document.addEventListener('keydown', handleKeyPress);
 
 setInterval(() => {
-    setStepToBlock({ keyCode: 40});  
+    operations[40]();
 }, 1000);
+
