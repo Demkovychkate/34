a18cdb0c2ec67af5627aa775e3fba207aecf774c
Demkovychkate <Panchevakate1804@gmail.com>
2023-11-17 23:55:27 +0200

    Making 1/2 of task

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const block = document.createElement('div');
+const blockWrapper = document.querySelector('.blockWrapper');
+const blockList = [];
+
+ const block = document.createElement('div');
 block.className = 'block';
-document.body.appendChild(block);
+blockList.push(block);
+blockWrapper.appendChild(block);
 
 const inputContainer = document.createElement('div');
 inputContainer.className = 'inputContainer';
@@ -16,61 +20,88 @@ scoreNumber.className = 'scoreNumber';
 scoreNumber.textContent = `Your Score`;
 inputContainer.appendChild(scoreNumber);
 
-
 const input = document.createElement('input');
 input.className = 'input';
 inputContainer.appendChild(input);
 
+const maxX = window.innerWidth - blockList[0].offsetWidth;
+const maxY = window.innerHeight - blockList[0].offsetHeight;
 
-
-const maxX = window.innerWidth - block.offsetWidth;
-const maxY = window.innerHeight - block.offsetHeight;
 const STEP = 50;
 
-let score = 0; 
+let score = 0;
 
 const operations = {
-    37: () => {
-        const leftPosition = parseInt(block.style.left || 0);
-        if (leftPosition - STEP >= 0 && isAboveBottomEdge()) {
-            block.style.left = `${leftPosition - STEP}px`;
-        } showScore();
-    },
-    39: () => {
-        const leftPosition = parseInt(block.style.left || 0);
-        if (leftPosition + STEP <= maxX && isAboveBottomEdge()) {
-            block.style.left = `${leftPosition + STEP}px`;
-        } showScore();
-    },
-    40: () => {
-         topPosition = parseInt(block.style.top || 0);
-        if (topPosition + STEP <= maxY) {
-            block.style.top = `${topPosition + STEP}px`;
-        } 
-    }
+    37: () => moveLeft(),
+    39: () => moveRight(),
+    40: () => moveDown(),
 };
 
-function isAboveBottomEdge() {
+function moveLeft() {
+    blockList.forEach((block) => {
+        const leftPosition = parseInt(block.style.left || 0);
+        if (leftPosition - STEP >= 0 && bottomEdge(block)) {
+            block.style.left = `${leftPosition - STEP}px`;
+            showScore();
+        }
+    });
+}
+
+function moveRight() {
+    blockList.forEach((block) => {
+        const leftPosition = parseInt(block.style.left || 0);
+        if (leftPosition + STEP <= maxX && bottomEdge(block)) {
+            block.style.left = `${leftPosition + STEP}px`;
+            showScore();
+        }
+    });
+}
+
+function moveDown() {
+    blockList.forEach((block) => {
+        const topPosition = parseInt(block.style.top || 0);
+        if (topPosition + STEP <= maxY && bottomEdge(block)) {
+            block.style.top = `${topPosition + STEP}px`;
+        }
+    });
+    createNewBlock();
+}
+
+function bottomEdge(block) {
     const topPosition = parseInt(block.style.top || 0);
     return topPosition + block.offsetHeight < maxY;
 }
 
 function showScore() {
-    if (isAboveBottomEdge()) {
+    const topBlock = blockList[blockList.length - 1];
+    if (bottomEdge(topBlock)) {
         score += 1;
         input.value = `${score}`;
     }
 }
 
-function handleKeyPress(e) {
+function createNewBlock() {
+    const bottomBlock = blockList[blockList.length - 1];
+    const topPosition = parseInt(bottomBlock.style.top || 0);
+    if (topPosition + STEP >= maxY) {
+        const newBlock = document.createElement('div');
+        newBlock.className = 'block';
+        blockList.push(newBlock);
+        blockWrapper.appendChild(newBlock);        
+    } 
+    
+}
+
+function keyPress(e) {
     if (operations[e.keyCode]) {
         operations[e.keyCode]();
     }
 }
 
-document.addEventListener('keydown', handleKeyPress);
+document.addEventListener('keydown', keyPress);
 
 setInterval(() => {
-    operations[40]();
+    moveDown();
 }, 1000);
+
 
